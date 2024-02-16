323fe943d554ccd28580b3345a8d8f4ebed348f4
shiopon01 <shio.0323@gmail.com>
2024-02-16 17:52:22 +0900

    add remarkBreaks plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
+import remarkBreaks from "remark-breaks";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.shiopon.net',
+	site: "https://blog.shiopon.net",
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkBreaks],
+		extendDefaultPlugins: true,
+	},
 });
