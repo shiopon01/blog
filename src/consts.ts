6a5df9abb004ad151b7007fcdc067866d6150f24
shiopon01 <shio.0323@gmail.com>
2026-01-20 00:01:24 +0900

    design update

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'shiopon blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'shiopon01のブログ。技術や日常について。';
